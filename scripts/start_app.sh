1398de518d1ab567a52b9e3fb51c612f75c85072
MuminekRS <66514639+MuminekRS@users.noreply.github.com>
2023-01-11 00:30:12 +0100

    Update start_app.sh

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 DIR="/home/ubuntu/Flask-App"
-if [ -d "$DIR" ]: then
+if [ -d "$DIR" ]; then
     echo "$DIR" exists:
 else
     echo "Creating $DIR directory"
     mkdir $DIR
 
-sudo chmod -R 777 /home/ubuntu/Flask-App
 cd /home/ubuntu/Flask-App
 sudo nohup python3 main.py > /dev/null 2>&1 &
