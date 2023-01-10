42347ea13216745d223fda8f42b223f06abe52b8
MuminekRS <66514639+MuminekRS@users.noreply.github.com>
2023-01-11 00:38:12 +0100

    req

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -2,9 +2,8 @@
 
 DIR="/home/ubuntu/Flask-App"
 if [ -d "$DIR" ]; then
-    echo "$DIR" exists:
     cd /home/ubuntu/Flask-App
-    sudonohup python3 main.py > /dev/null 2>&1 &
+    sudo nohup python3 main.py > /dev/null 2>&1 &
 else
     echo "Creating $DIR directory"
     mkdir $DIR
