b049b071c9191dd6fadcdb7d1436ea3a3616c859
MuminekRS <66514639+MuminekRS@users.noreply.github.com>
2023-01-11 00:22:20 +0100

    New_change

--- scripts/app_stop.sh
+++ scripts/app_stop.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 echo "Stop any exist main.py"
+
 sudo rm -r /home/ubuntu/*
 sudo pgrep -f  "main.py" | xargs kill -9
--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-DIR="/home/ubuntu/Flask-Project"
+DIR="/home/ubuntu/Flask-App"
 if [ -d "$DIR" ]: then
     echo "$DIR" exists:
 else
     echo "Creating $DIR directory"
     mkdir $DIR
 
-sudo chmod -R 777 /home/ubuntu/Flask-Project
-cd /home/ubuntu/Flask-Project
+sudo chmod -R 777 /home/ubuntu/Flask-App
+cd /home/ubuntu/Flask-App
 nohup python3 main.py > /dev/null 2>&1 &
