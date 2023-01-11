562d64e162fac1af1fa7f9f3892731ba9183dd3e
MuminekRS <66514639+MuminekRS@users.noreply.github.com>
2023-01-11 16:17:34 +0100

    req

--- scripts/app_stop.sh
+++ scripts/app_stop.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+DIR="/home/ubuntu/Flask-App"
 
 echo "Stop any exist main.py"
+DIR="/home/ubuntu/Flask-App"
+if [ -d "$DIR" ]; then
+    echo "$DIR" exist:
+else
+    echo "Creating $DIR directory"
+    mkdir $DIR
+fi
+
 
 
 ps -auf | grep main.py | sudo awk  '{system("sudo kill -9 "$2)}' > /dev/null 2>&1 &
--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-DIR="/home/ubuntu/Flask-App"
-if [ -d "$DIR" ]; then
-    cd /home/ubuntu/Flask-App
-    sudo nohup python3 main.py > /dev/null 2>&1 &:
-else
-    echo "Creating $DIR directory"
-    mkdir $DIR
-fi
 
+
+cd /home/ubuntu/Flask-App
+sudo nohup python3 main.py > /dev/null 2>&1 &:
