107fca1f05d947ebffb8784aeb6543c73f9d5988
MuminekRS <66514639+MuminekRS@users.noreply.github.com>
2023-01-12 12:00:12 +0100

    New_sh_file

--- scripts/app_stop.sh
+++ scripts/app_stop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIR="/home/ubuntu/Flask-App"
 
-echo "Stop any exist main.py"
+
 DIR="/home/ubuntu/Flask-App"
 if [ -d "$DIR" ]; then
     echo "$DIR exist"
@@ -9,3 +9,8 @@ else
     echo "Creating $DIR directory"
     mkdir $DIR
 fi
+
+echo "Stop any exist main.py"
+
+sudo rm -r /home/ubuntu/*
+sudo ps -auf | grep main.py | sudo awk  '{system("sudo kill -9 "$2)}' > /dev/null 2>&1 &
